5b14daea0f9d724e32efcd6ae8848400e9b677f1
Nabeel <mhdnabeelkp6056@gmail.com>
2026-05-21 12:45:35 +0530

    make this clone fully responsive and add menu bar icon and drop down menu function in js

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector(".menu-icon")
+let navLinks = document.querySelector(".nav-links")
+
+menuIcon.addEventListener("click", function(){
+
+    navLinks.classList.toggle("active")
+
+})
